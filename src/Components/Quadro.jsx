6cfb605b76a21c5c20cde77a6cf3a82473c82ae6
Francisco <113522254+WillianXG@users.noreply.github.com>
2023-01-26 10:35:42 -0300

    Estilização

---
+++ src/Components/Quadro.jsx
@@ -0,0 +1,21 @@
+import "./Quadro.css"
+
+export default function Quadro() {
+    return (
+        <>
+            <div className="painel">
+                <div className="retangulo">
+                    <div className="cartaz">
+                        <h1>Ola</h1>
+                    </div>
+                    <br></br>
+                    <h3>Filme:</h3>
+                    <h3>Descrição:</h3>
+                    <h3>Preço:</h3>
+                </div>
+            </div>
+            <br>
+            </br>
+        </>
+    )
+}
